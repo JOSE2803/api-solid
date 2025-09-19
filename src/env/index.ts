b6f9b93e6564ee8bca772e270f61b770367d8a2a
Jose Lacerda <evaristolacerda@gmail.com>
2025-09-19 00:06:43 -0300

    feat: integrate Prisma for database management and add user model

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,20 +1,19 @@
-import 'dotenv/config';
-import { z } from 'zod';
+import 'dotenv/config'
+import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'prod']).default('dev'),
   PORT: z.coerce.number().default(3333),
-});
+})
 
 const _env = envSchema.safeParse({
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
-});
+})
 
 if (!_env.success) {
-  console.error('❌ Invalid environment variables');
-  throw new Error('Invalid environment variables.');
+  console.error('❌ Invalid environment variables')
+  throw new Error('Invalid environment variables.')
 }
 
 export const env = _env.data
-
